99444a89b6922557a87d5073de5facdfeba496f3
chwflhs <34463825+chwflhs@users.noreply.github.com>
2026-01-23 23:02:22 +0800

    Update 5x5_evaluate.bat

--- MAPOLight/5x5_evaluate.bat
+++ MAPOLight/5x5_evaluate.bat
@@ -1,5 +1,5 @@
 @echo off
-echo "! 该脚本中的检查点路径需要根据实际情况配置 !"
+echo "! The checkpoint path in this script needs to be configured according to your specific environment!"
 
 python test2.py -a a3c -c "5x5_weights/A3C/1/checkpoint_019043" -p 1 --co -n /sumo_files/5_2net.net.xml -r /sumo_files/5_2groutes.xml --cfg /sumo_files/5_2grid.sumocfg -o 5x5 -t 4800 --128width
 python test2.py -a a3c -c "5x5_weights/A3C/0.05/checkpoint_014438" -p 0.05 --co -n /sumo_files/5_2net.net.xml -r /sumo_files/5_2groutes.xml --cfg /sumo_files/5_2grid.sumocfg -o 5x5 -t 4800 --128width
